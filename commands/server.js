f8edb7b11a4d3945fef9182debcfadf5bf9539a1
Bharat Kathi <bharat1031@gmail.com>
2020-12-22 22:27:18 -0800

    updated command to include ftb smp status

--- commands/server.js
+++ commands/server.js
@@ -22,7 +22,7 @@ module.exports = {
                     // Server is online
                     console.log('Server is online!');
                     message.channel.send(new Discord.RichEmbed()
-                        .setTitle('BK1031 SMP')
+                        .setTitle('Vanilla SMP')
                         .setDescription('mc.bk1031.dev')
                         .setColor('#42f477')
                         .addField('Version', body.version)
@@ -35,8 +35,34 @@ module.exports = {
                 else {
                     console.log('Server not online!');
                     message.channel.send(new Discord.RichEmbed()
-                        .setTitle('BK1031 SMP')
+                        .setTitle('Vanilla SMP')
                         .setDescription('mc.bk1031.dev')
+                        .setColor('#f44242')
+                        .addField('Offline', `--------`)
+                        .setTimestamp()
+                    );
+                }
+            });
+            request('https://api.mcsrvstat.us/2/ftb.bk1031.dev', {json:true}, (err, res, body) => {
+                if (body.online) {
+                    // Server is online
+                    console.log('Server is online!');
+                    message.channel.send(new Discord.RichEmbed()
+                        .setTitle('FTB Revelation SMP')
+                        .setDescription('ftb.bk1031.dev')
+                        .setColor('#42f477')
+                        .addField('Version', body.version)
+                        .addField('Online', `${body.players.online}/${body.players.max}`)
+                        .addField('Players', body.players.list)
+                        .setFooter(body.software)
+                        .setTimestamp()
+                    );
+                }
+                else {
+                    console.log('Server not online!');
+                    message.channel.send(new Discord.RichEmbed()
+                        .setTitle('FTB Revelation SMP')
+                        .setDescription('ftb.bk1031.dev')
                         .setColor('#f44242')
                         .addField('Offline', `--------`)
                         .setTimestamp()
