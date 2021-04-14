058ce5282a82463f79ffbe2677b841b231c79cda
Bharat Kathi <bharat1031@gmail.com>
2021-04-14 15:18:47 -0700

    fixed server message

--- commands/server.js
+++ commands/server.js
@@ -5,6 +5,6 @@ module.exports = {
 	name: 'server',
 	description: 'Get SMP server status',
 	execute(message, args) {
-        message.channel.send("We do not have a minecraft server currently running! If you would like to see one, please leave a comment in <#744105753387991040>");
+        message.channel.send("We do not have a minecraft server currently running! If you would like to see one, please leave a comment in <#662099377061625869>");
 	},
 };
